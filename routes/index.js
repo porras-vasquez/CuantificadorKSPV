3bbf51923d8d45d474697d0a68e774ffeaf89747
christopher4273 <c.andrey.a.g@gmail.com>
2021-11-17 05:44:41 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+const isAuthenticated= require('../helpers/auth');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',isAuthenticated, function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 router.get('/navbar', function(req, res) {
