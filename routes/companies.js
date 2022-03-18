6a6956fdd2ba56a39bd2e1b0c611774f72e121ce
Enma123 <joseemiliogr5@gmail.com>
2022-03-18 10:09:58 -0600

    10:09

--- routes/companies.js
+++ routes/companies.js
@@ -10,7 +10,7 @@ router.get('/createCompany', isAuthenticated,function(req, res) {
 });
 
 router.get('/searchCompany/:id',isAuthenticated, companyController.search);
-router.get('/showCompany', isAuthenticated,isAuthenticated,companyController.list);
+router.get('/showCompany',isAuthenticated,companyController.list);
 //router.get('/showCompany2',isAuthenticated, companyController.list2);
 
 router.post('/saveCompany', isAuthenticated,companyController.save);
