0d2e2b79bdffaa0abb4e1f7bf85f6cbc3f355d9c
Enma123 <joseemiliogr5@gmail.com>
2022-03-18 15:49:18 -0600

    15:49

--- controllers/CompanyController.js
+++ controllers/CompanyController.js
@@ -2,15 +2,42 @@
 require('../connection');
 var Company = require("../models/Company");
 var companyController = {};
+var status = 0;
+var message = "";
+
+function verifyStatus(statusCode){
+    if(statusCode==200){//Satisfactorio
+        status=200;
+        message="¡Realizado exitosamente!";
+    }else if(statusCode==400){//Solicitud incorrecta
+        status=400;
+        message="¡Error, solicitud incorrecta!";
+    }else if(statusCode==401){//No autenticado
+        status=401;
+        message="¡Error, usuario no autenticado!";
+    }else if(statusCode==404){//No encontrado
+        status=404;
+        message="¡Ocurrió un problema con la ruta de acceso!";
+    }else if(statusCode==500){//Error del servidor
+        status=500;
+        message="¡Lo sentimos, ocurrió un problema con el servidor!";
+    }else if(statusCode==503){//Mantenimiento
+        status=503;
+        message="¡Lo sentimos, el servidor se encuentra en mantenimiento!";
+    }
+}
+
 //Guardar
 companyController.save = async function (req, res) {
     var company = new Company(req.body);
     await company.save(function (err) {
         if (err) {
-            res.render('../views/company/NewCompany', { message: "error" });
+            verifyStatus(res.statusCode);
+            res.render('../views/company/NewCompany', { status: status, message: message});
         }
         else {
-            res.render('../views/company/NewCompany', { message: "success" });
+            verifyStatus(res.statusCode);
+            res.render('../views/company/NewCompany', { status: status, message: message });
         }
 
     });
@@ -46,24 +73,48 @@ companyController.update = function (req, res) {
     }, { new: true },
         function (err, company) {
             if (err) {
-                console.log('Error: ', err);
-                req.flash("error_msg", "error_update");
-                res.redirect('/companies/showCompany');
-
+                verifyStatus(res.statusCode);
+                Company.find({}).exec(function (err, companies) {
+                    if (err) { 
+                        res.render('../views/company/AllCompanies', { companies: companies, status: status, message: message });
+                    }else{
+                        res.render('../views/company/AllCompanies', { companies: companies, status: status, message: message });
+                    }
+                });
+            }else{
+                verifyStatus(res.statusCode);
+                Company.find({}).exec(function (err, companies) {
+                    if (err) { 
+                        res.render('../views/company/AllCompanies', { companies: companies, status: status, message: message });
+                    }else{
+                        res.render('../views/company/AllCompanies', { companies: companies, status: status, message: message });
+                    }
+                });
             }
-            req.flash("success_msg", "updated");
-            res.redirect('/companies/showCompany');
         });
 };
 //Eliminar
 companyController.delete = function (req, res) {
     Company.deleteOne({ _id: req.params.id }, function (err) {
         if (err) {
-            //agregar error'
-            req.flash("error_msg", "error_delete");
+            verifyStatus(res.statusCode);
+            Company.find({}).exec(function (err, companies) {
+                if (err) { 
+                    res.render('../views/company/AllCompanies', { companies: companies, status: status, message: message });
+                }else{
+                    res.render('../views/company/AllCompanies', { companies: companies, status: status, message: message });
+                }
+            });
+        }else{
+            verifyStatus(res.statusCode);
+            Company.find({}).exec(function (err, companies) {
+                if (err) { 
+                    res.render('../views/company/AllCompanies', { companies: companies, status: status, message: message });
+                }else{
+                    res.render('../views/company/AllCompanies', { companies: companies, status: status, message: message });
+                }
+            });
         }
-        req.flash("success_msg", "deleted");
-        res.redirect("/companies/showCompany");
     });
 
 };
