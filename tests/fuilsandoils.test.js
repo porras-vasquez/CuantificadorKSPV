11dd1521dfc0951ca09a1c90a64f26d47301d0bd
Marvin Juarez Aguilar <68169947+R3IZA@users.noreply.github.com>
2022-03-26 20:35:00 -0600

    fjoifrjn

--- tests/fuilsandoils.test.js
+++ tests/fuilsandoils.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 const app = require('../app')
 
-it('respond with json containing a list of all electricities', () =>{
+it('respond with json containing a list of all fuels and oil', () =>{
     request(app)
     .get('/fuelsAndOil/fuelsAndOilShow/6234bde44d54cf47a0b734c1')
         .set('Accept', 'application/json')
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest')
 const app = require('../app')
 
+/*SHOW*/
 it('respond with json containing a list of all users', () =>{
     request(app)
     .get('/users/show')
@@ -8,3 +9,19 @@ it('respond with json containing a list of all users', () =>{
             .expect('Content-Type', /json/)
                 .expect(200);
 });
+/*ADD*/
+/*it('save all the information of user', () =>{
+    request(app)
+    .save('/users/show')
+        .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+                .expect(200);
+});
+
+it("should be able to add and complete TODOs", function() {
+    let todos = new Todos();
+    todos.add("get up from bed");
+    todos.add("make up bed");
+    assert.strictEqual(todos.list().length, 0);
+});*/
+
