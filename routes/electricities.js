1b9d5b0094c21baa1e386e492239d6b38bc863cd
Enma123 <joseemiliogr5@gmail.com>
2022-05-14 21:24:29 -0600

    21:24

--- routes/electricities.js
+++ routes/electricities.js
@@ -11,20 +11,18 @@ router.get('/createElectricity',isAuthenticated, function(req, res) {
 
 //RENDERIZAN PÁGINAS
 router.get('/electricity/:comp',isAuthenticated, electricityController.renderPageNewElectricity);
-router.get('/editElectricity/:id', electricityController.renderPageEditElectricity);
-router.get('/electricities/:comp', electricityController.renderPageAllElectricites, (req, res) =>{
-    return res.json("all electricity sent"); 
-});
+router.get('/editElectricity/:id',isAuthenticated, electricityController.renderPageEditElectricity);
+router.get('/electricities/:comp',isAuthenticated, electricityController.renderPageAllElectricites);
 router.get('/newMeter/:id/:comp', isAuthenticated, electricityController.renderPageNewMeter);
-router.get('/editMeter/:elec/:meter', electricityController.renderPageEditMeter);
-router.get('/meters/:id', electricityController.renderPageAllMeters);
+router.get('/editMeter/:elec/:meter',isAuthenticated, electricityController.renderPageEditMeter);
+router.get('/meters/:id',isAuthenticated, electricityController.renderPageAllMeters);
 
 //ELIMINAN, GUARDAN O ACTUALIZAN DATOS
 router.post('/updateElectricity/:id', isAuthenticated, electricityController.update);
-router.post('/saveElectricity/:comp', electricityController.save);
-router.post('/deleteElectricity/:id/:comp', electricityController.delete);
-router.post('/addMeter/:_id/:comp', electricityController.addMeter);
+router.post('/saveElectricity/:comp',isAuthenticated, electricityController.save);
+router.post('/deleteElectricity/:id/:comp',isAuthenticated, electricityController.delete);
+router.post('/addMeter/:_id/:comp',isAuthenticated, electricityController.addMeter);
 router.post('/updateMeter/:elec/:meter', isAuthenticated, electricityController.updateMeter);
-router.post('/deleteMeter/:elec/:meter', electricityController.deleteMeter);
+router.post('/deleteMeter/:elec/:meter',isAuthenticated, electricityController.deleteMeter);
 //router.post('/updateElectricity/:id', electricityController.update);
 module.exports = router; 
